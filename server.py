57963cebb2ed3788d42233f440cbb25c94767c29
Adarsh <2017csb1064@iitrpr.ac.in>
2019-10-26 14:42:18 +0530

    Server Up and Running

---
+++ server.py
@@ -0,0 +1,15 @@
+import http.server
+import socketserver
+import os
+
+web_dir = os.path.join(os.path.dirname(__file__), 'main_app\\templates\\')
+os.chdir(web_dir)
+
+PORT = 8000
+Handler = http.server.SimpleHTTPRequestHandler
+
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Server at port: ", PORT)
+    print("Quit the server by Ctrl+C")
+    httpd.serve_forever()
