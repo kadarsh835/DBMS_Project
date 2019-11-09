5fc45d4dc19b89f689465e3e79108f13236dab4d
Adarsh <2017csb1064@iitrpr.ac.in>
2019-11-09 23:44:40 +0530

    gitignore config

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import http.server
 import socketserver
 import os
-from main_app import config
+from config import *
+# import config as config
 
 web_dir = os.path.join(os.path.dirname(__file__), 'main_app\\templates\\')
 os.chdir(web_dir)
@@ -11,7 +12,7 @@ Handler = http.server.SimpleHTTPRequestHandler
 
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    config.connect()
+    connect()
     print("Server at port: ", PORT)
     print("Quit the server by Ctrl+C")
     httpd.serve_forever()
